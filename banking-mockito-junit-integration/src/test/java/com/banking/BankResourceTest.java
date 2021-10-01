2bde9f2707dcf1754301511368142c380ba9c825
sam253narula <samarthnarula253@gmail.com>
2021-10-01 08:40:21 +0530

    small channges

--- banking-mockito-junit-integration/src/test/java/com/banking/BankResourceTest.java
+++ banking-mockito-junit-integration/src/test/java/com/banking/BankResourceTest.java
@@ -3,15 +3,12 @@ package com.banking;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import com.banking.Account;
-import com.banking.AccountResource;
-import com.banking.AccountService;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BankResourceTest {
@@ -21,6 +18,7 @@ public class BankResourceTest {
 
 	// Method of mocking without using @mock annotation
 	// private accountService accountService = Mockito.mock(accountService.class);
+	
 	private AccountResource accountResource;
 
 	@Before
@@ -28,15 +26,20 @@ public class BankResourceTest {
 		accountResource = new AccountResource(accountService);
 	}
 
+	//Alternate method of loading before the class loads. By using @injectMocks
+	
+//	@InjectMocks
+//	AccountResource accountResource;
+	
 	@Test
 	public void shouldSaveaccount() {
 		Account account = new Account("987654321", 20000, "Ram Charan", Type.CURRENT);
 		Mockito.when(accountService.saveaccount(account)).thenReturn(1);
 		/*
 		 * if you want mockito to match against any account object and not just above
-		 * account object,, then u can use Matchers.any(account.class)
+		 * account object,, then u can use Matchers.any(Account.class)
 		 */
-		// Mockito.when(accountService.saveaccount(Matchers.any(account.class))).thenReturn(1);
+		// Mockito.when(accountService.saveaccount(Matchers.any(Account.class))).thenReturn(1);
 		int id = accountResource.saveNewAccount("987654321", 20000, "Ram Charan", Type.CURRENT);
 		Assert.assertEquals(1, id);
 
@@ -59,8 +62,13 @@ public class BankResourceTest {
 		Mockito.verify(accountService, Mockito.atMost(3)).logInfo();
 		Mockito.verify(accountService, Mockito.atLeastOnce()).logInfo();
 		Mockito.verify(accountService, Mockito.atMostOnce()).logInfo();
-
+		
+		
+		// Below is from Junit 4
 		Assert.assertEquals(1, id);
+		
+		//Below is from Junit 5
+		Assertions.assertEquals(1, id);
 
 	}
 
